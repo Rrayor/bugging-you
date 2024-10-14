32708c48330021b1c6a5b916f634c1ed8782edc5
Benjamin Simon <simon.benjamin93@gmail.com>
2024-10-14 23:48:40 +0200

    Basic notifications

--- app/src/main/java/com/rrayor/buggingyou/MainActivity.kt
+++ app/src/main/java/com/rrayor/buggingyou/MainActivity.kt
@@ -1,47 +1,112 @@
 package com.rrayor.buggingyou
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.rrayor.buggingyou.ui.theme.MyApplicationTheme
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.rrayor.buggingyou.ui.theme.BuggingYouTheme
 
 class MainActivity : ComponentActivity() {
+    val CHANNEL_ID = "bugging_you"
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val notification = createNotification()
         enableEdgeToEdge()
+        createNotificationChannel()
         setContent {
-            MyApplicationTheme {
+            BuggingYouTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
+        }
+
+        sendNotification(0, notification)
+    }
+
+    private fun sendNotification(notificationId: Int, notification: Notification) {
+        with(NotificationManagerCompat.from(this)) {
+            if (ActivityCompat.checkSelfPermission(
+                    this@MainActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                    isGranted -> if (isGranted) {
+                        sendNotification(notificationId, notification)
+                    }
+                }
+
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+
+                return@with
+            }
+            // notificationId is a unique int for each notification that you must define.
+            notify(notificationId, notification)
+        }
+    }
+
+    private fun createNotification(): Notification {
+        var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Title")
+            .setContentText("Content")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        return builder.build()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyApplicationTheme {
+    BuggingYouTheme() {
         Greeting("Android")
     }
 }
